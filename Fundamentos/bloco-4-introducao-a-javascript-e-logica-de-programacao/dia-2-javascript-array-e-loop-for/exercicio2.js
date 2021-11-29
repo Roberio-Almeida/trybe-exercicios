88c62c880016925a5b6e6ef4fceefe2fea8b097d
Robério Almeida <dev.roberio@gmail.com>
2021-11-29 19:50:32 -0300

    exercise 1 to 7 completed

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio2.js
@@ -0,0 +1,56 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/* exercise 1
+for(let i of numbers) {
+    console.log(i);
+}*/
+
+/* exercise 2
+let sum = 0;
+for(let i of numbers) {
+    sum = sum + i;
+}
+console.log(sum);*/
+
+/* exercise 3 e 4
+let sum = 0;
+for(let i of numbers) {
+    sum = sum + i / numbers.length;
+} if (sum > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}*/
+
+/* exercise 5
+let biggest = 0;
+for (let i = 0; i < numbers.length; i+=1) {
+   if (numbers[i] > biggest) {
+    biggest = numbers[i];
+   }
+}
+console.log(biggest);*/
+
+/* exercise 6
+let odd = 0;
+for(let i = 0; i < numbers.length; i+=1) {
+    if (numbers[i] % 2 > 0) {
+        odd+=1;
+    }
+} 
+    if (odd > 0) {
+        console.log(odd);
+    }
+    else {
+        console.log("nenhum valor ímpar encontrado");
+}*/
+
+/* exercise 7
+let lowest = numbers[0];
+for(let i = 0; i < numbers.length; i+=1){
+    if(numbers[i] < lowest){
+        lowest = numbers[i];   
+    }
+}
+console.log(lowest);*/
+

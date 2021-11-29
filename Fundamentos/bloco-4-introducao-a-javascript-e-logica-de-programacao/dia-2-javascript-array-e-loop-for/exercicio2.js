c275f862c1752f2dfe9b67396a93c78682a24e0f
Robério Almeida <dev.roberio@gmail.com>
2021-11-29 20:02:51 -0300

    status final

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio2.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio2.js
@@ -3,16 +3,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 /* exercise 1
 for(let i of numbers) {
     console.log(i);
-}*/
+}
 
-/* exercise 2
+exercise 2
 let sum = 0;
 for(let i of numbers) {
     sum = sum + i;
 }
-console.log(sum);*/
+console.log(sum);
 
-/* exercise 3 e 4
+exercise 3 e 4
 let sum = 0;
 for(let i of numbers) {
     sum = sum + i / numbers.length;
@@ -20,18 +20,18 @@ for(let i of numbers) {
     console.log("valor maior que 20");
 } else {
     console.log("valor menor ou igual a 20");
-}*/
+}
 
-/* exercise 5
+exercise 5
 let biggest = 0;
 for (let i = 0; i < numbers.length; i+=1) {
    if (numbers[i] > biggest) {
     biggest = numbers[i];
    }
 }
-console.log(biggest);*/
+console.log(biggest);
 
-/* exercise 6
+exercise 6
 let odd = 0;
 for(let i = 0; i < numbers.length; i+=1) {
     if (numbers[i] % 2 > 0) {
@@ -43,21 +43,21 @@ for(let i = 0; i < numbers.length; i+=1) {
     }
     else {
         console.log("nenhum valor ímpar encontrado");
-}*/
+}
 
-/* exercise 7
+exercise 7
 let lowest = numbers[0];
 for(let i = 0; i < numbers.length; i+=1){
     if(numbers[i] < lowest){
         lowest = numbers[i];   
     }
 }
-console.log(lowest);*/
+console.log(lowest);
 
-/* exercise 8 e 9
+exercise 8 e 9*/
+
 let oneTo25 = [];
 for(let i = 1; i <= 25; i+=1) {
     oneTo25.push(i / 2);
 }
-console.log(oneTo25);*/
-
+console.log(oneTo25);

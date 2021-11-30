1018c0a9b1bdfb5b4b88393348a64848e9b7be4a
Robério Almeida <dev.roberio@gmail.com>
2021-11-30 19:02:47 -0300

    exercise 1 and 2 completed, but i left exercise 3+ for the future Robério

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercise.js
@@ -0,0 +1,21 @@
+let n = 5;
+let square = "";
+let space = n;
+
+/* exercise 1
+for(let i = 0; i < n; i+=1) {
+    square+= "*";
+}
+for(let i = 0; i < n; i+=1){
+    console.log(square);
+}*/
+
+/* exercise 2
+for(let i = 0; i < n; i+=1){
+    square += "*";
+    console.log(square);
+}*/
+
+for(let i = 0; i < 4; i+=1) {
+
+}
